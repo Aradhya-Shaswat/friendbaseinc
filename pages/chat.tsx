c3d881fc9ee7e3bf836735d339de889c9c36498d
Twlight_zone <twlightzonehelp@gmail.com>
2022-07-31 14:20:01 +0530

    doing nothing

--- pages/chat.tsx
+++ pages/chat.tsx
@@ -11,7 +11,7 @@ interface IProps {
   isPostingComment: Boolean;
   comment: string;
   setComment: Dispatch<SetStateAction<string>>;
-  addComment: (e: React.FormEvent) => typeof addComment;
+  addComment: (e: React.FormEvent) => void;
   comments: IComment[];
 }
 
@@ -22,12 +22,12 @@ interface IComment {
   postedBy: { _ref?: string; _id?: string };
 }
 
-function addComment(){
-// this is OG at its finest
-}
-
 const Chat = ({ comment, setComment, addComment, comments, isPostingComment }: IProps) => {
   const { allUsers, userProfile }: any = useAuthStore();
+
+  setComment=()=>{
+    console.log("haha cant catch me now idiots!")
+  }
 
   return (
     <div className='border-t-2 border-gray-200 pt-4 px-10 mt-4 bg-[#F8F8F8] border-b-2 lg:pb-0 pb-[100px]'>
@@ -69,23 +69,22 @@ const Chat = ({ comment, setComment, addComment, comments, isPostingComment }: I
             </>
           ))
         ) : (
-          <NoResults text='No Chat! Add One and boom!'/>
+          <NoResults text='No Comments Yet! Be First to do add the comment.' />
         )}
       </div>
-        <div className='justify-center align-center'>
-            {userProfile && <div className='absolute bottom-0 left-0  pb-6 px-2 md:px-10 '>
-            <form onSubmit={addComment} className='flex gap-4'>
-            <input
-                value={comment}
-                className='bg-primary px-6 py-4 text-md font-medium border-2 w-[250px] md:w-[700px] lg:w-[350px] border-gray-100 focus:outline-none focus:border-2 focus:border-gray-300 flex-1 rounded-lg'
-                placeholder='Usage of Space not allowed.'
-             />
-            <button className='text-md text-gray-400 ' onClick={addComment}>
-                {isPostingComment ? 'Sending...' : 'Comment'}
-            </button>
-            </form>
-            </div>}
-        </div>
+     {userProfile && <div className='absolute bottom-0 left-0  pb-6 px-2 md:px-10 '>
+        <form onSubmit={addComment} className='flex gap-4'>
+          <input
+            value={comment}
+            onChange={(e) => setComment(e.target.value.trim())}
+            className='bg-primary px-6 py-4 text-md font-medium border-2 w-[250px] md:w-[700px] lg:w-[350px] border-gray-100 focus:outline-none focus:border-2 focus:border-gray-300 flex-1 rounded-lg'
+            placeholder='Usage of Space not allowed.'
+          />
+          <button className='text-md text-gray-400 ' onClick={addComment}>
+            {isPostingComment ? 'Commenting...' : 'Comment'}
+          </button>
+        </form>
+      </div>}
     </div>
   );
 };
