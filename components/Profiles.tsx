a9d70c65e21e08efa3ba6768045a918fd10de552
Twlight_zone <twlightzonehelp@gmail.com>
2022-08-11 15:20:56 +0530

    I dont think this works fully (DUE TO U NOT GIVING ME THE .evn SO FUCK OFF M8)

---
+++ components/Profiles.tsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import {View, Text, SafeAreaView} from 'react-native';
+// TODO All the `declare module 'react-native';` to the types.d.ts :)
+
+const Profiles = () => {
+    return(
+        /*
+        TODO Gimme the .env.development or *else*
+        ! NOT SAFE CODE PLZ SEE FOR ERRORS BEFORE RUNNING
+        */
+       <View>
+        <SafeAreaView />
+        <Text>Profile Screen</Text>
+       </View>
+    )
+}
+
+export default Profiles;
